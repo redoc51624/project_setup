dfc8bc0303bf696f6b41ffb3cbd5de96cf431697
vikas kumar <vikas10145.12@bitmesra.ac.in>
2020-07-31 17:39:48 +0530

    Backened data integration
    -JSON data is coming from backened server.js in about page
    - Home page and contact page data is from same page

--- server.js
+++ server.js
@@ -1,14 +1,28 @@
-// if(process.env.NODE_ENV !== "production") {
-//     require('dotenv').load()
-// }
+if(process.env.NODE_ENV !== "production") {
+    require('dotenv').config()
+}
 
 const stripeSecretKey = process.env.STRIPE_SECRET_KEY
 console.log(stripeSecretKey)
 
 const express = require("express")
 const app = express()
+const fs = require('fs')
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
+
+app.get('/about', function(req,res){
+    fs.readFile('data.json', function(error, data){
+        if(error)
+        {
+            res.status(500).end()
+        } else {
+            res.render('about.ejs', {
+                items: JSON.parse(data)
+            })
+        }
+    })
+})
 
 app.listen(3000)
